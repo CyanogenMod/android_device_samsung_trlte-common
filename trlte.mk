d2327c953ce4fb998518f7dda4a922fd338e49c5
gekkehenkie11 <allard_addink@hotmail.com>
2015-07-16 09:48:36 -0400

    trlte-common: dont need FFMPEG config anymore
    Change-Id: Ie3022c55092555dc3536c3c71d08243c0ccda305

--- trlte.mk
+++ trlte.mk
@@ -171,7 +171,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Power HAL
 PRODUCT_PACKAGES += \
